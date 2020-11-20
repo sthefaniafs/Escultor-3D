edf2260d5fc5af10f259bcb3654695532f5fc647
Sthefania <sthefaniafernandes03@gmail.com>
2020-11-20 10:51:56 -0300

    Update voxel.hpp

--- voxel.hpp
+++ voxel.hpp
@@ -2,10 +2,36 @@
 #ifndef VOXEL_HPP
 #define VOXEL_HPP
 
+/**
+ * @brief A struct Voxel desempenha o papel de um pixel 3D
+ * - Para isso, a struct conta com a capacidade de armazenar informações como cor e transparência, necessárias para idealizar os elementos de uma escultura. As cores são representadas numa faixa de [0,1], onde 0 denota ausência total da componente e 1 denota a presença total da componente de cor. Na transparência os valores também devem está na faixa [0,1], onde 0 denota transparência total e 1 denota total opacidade. Além disso, a classe possui um booleano usado para ativar ou não o voxel.
+ */
 struct Voxel {
-  float r,g,b; // cores (vermelho, verde, azul)
-  float a; // Transparência (1 opaco, 0 transparente)
-  bool isOn; // Included or not
+  /**
+   * @brief \b r armazena o valor correspondente a cor vermelha
+   * 
+   */
+  float r;
+  /**
+   * @brief \b g armazena o valor correspondente a cor verde
+   * 
+   */
+  float g;
+  /**
+   * @brief \b b armazena o valor correspondente a cor azul
+   * 
+   */
+  float b;
+  /**
+   * @brief \b a armazena o valor correspondente a transparência do desenho 
+   * 
+   */
+  float a; 
+  /**
+   * @brief \b isOn define se o voxel correspondente deve ser incluído no arquivo digital 
+   * 
+   */
+  bool isOn;
 };
 #endif // VOXEL_HPP
 
